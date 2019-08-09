d1ee91cd09bb86814d012394f31f5f17070bf14d
neorobin <neorobin@163.com>
2019-08-10 00:38:56 +0800

    Update pin_clock_WIN10_1.cmd

--- pin_clock_WIN10_1.cmd
+++ pin_clock_WIN10_1.cmd
@@ -1,57 +1,45 @@
 @echo off & setlocal enabledelayedexpansion
-
 chcp 437
 
-set "Path=%SystemRoot%\system32" & for /f "delims==" %%a in ('set') do if /i "%%a" neq "Path" set "%%a="
+(for /f "delims==" %%a in ('set') do set "%%a=") & set "Path=%SystemRoot%\system32"
 
 for /F %%a in ('echo prompt $E^| cmd') do set "_ESC=%%a"
 
-set /a "_PI=31416, _2PI=2*_PI, _PI#2=_PI/2, _3PI#2=3*_PI/2, _3PI#2_1=_3PI#2-1, _DEG=_PI/180, _6DEG=6*_PI/180"
+set /a "_WID=75, _HEI=75, _HALF_WID = _WID / 2 - 3"
+color 0c & mode !_WID!,!_HEI!
+set "Path="
+
+
 set "_SIN=(t-t*t/1875*t/320000+t*t/1875*t/15625*t/16000*t/2560000-t*t/1875*t/15360*t/15625*t/15625*t/16000*t/44800000)"
 set "_COS=(10000-t*t/20000+t*t/1875*t/15625*t/819200-t*t/1875*t/15360*t/15625*t/15625*t/10240000+t*t/1875*t/15360*t/15625*t/15625*t/16000*t/15625*t/229376000)"
 
-set /a "_WID=75, _HEI=75, _iMax=_WID*_HEI, _HALF_WID = _WID / 2 - 3"
-color 0c & mode !_WID!,!_HEI!
-
-REM (for /l %%i in (1 1 !_iMax!) do set "scr= !scr!") & set "emp=!scr!"
-
-REM set /a "buffwid = _WID, linesWantBackAbove = _HEI - 1, cntBS = 2 + (buffwid + 7) / 8 * linesWantBackAbove"
-
-REM set "BSs=" & for /L %%a in (1 1 !cntBS!) do set "BSs=!BSs!%BS%"
-REM set "aLineBS=" & for /L %%a in (1 1 !_WID!) do set "aLineBS=!aLineBS!%BS%"
-
-set /a "_XC =10000* _WID/2, _YC =10000* _HEI/2, _TH0=!random! %% 360 * %_DEG%"
+set /a "_PI=31416, _2PI=2*_PI, _PI#2=_PI/2, _3PI#2=3*_PI/2, _3PI#2_1=_3PI#2-1, _DEG=_PI/180"
 
 
-<nul set /p "=%_ESC%[!p"
-<nul set /p "=%_ESC%[?25l"
+set /a "_XC = 10000 * _WID/2, _YC = 10000 * _HEI/2, _TH0=!random! %% 360 * %_DEG%"
 
-REM _ESC [ ? 25 l	DECTCEM	Text Cursor Enable Mode Hide	Hide the cursor
+
+REM <nul set /p "=%_ESC%[!p"
+
+<nul set /p "=%_ESC%[?25l" & REM _ESC [ ? 25 l	DECTCEM	Text Cursor Enable Mode Hide	Hide the cursor
 
 set "$erase_last_pin="
-
-set /a "_SPEED=1*%_DEG%, th=_TH0+%_2PI%, _DTH=-_SPEED"
-
-
-
-set /a "hue=88"
+set /a "_SPEED=3*%_DEG%, th=_TH0+%_2PI%, hue=88, _DTH=-_SPEED"
 
 (
 	for /f "delims==" %%a in ('set _') do set "%%a="
-	set "PATH="
 
 	for /L %%i in (0 1 500000) do (
 
-		set /a "th+=-%_SPEED%+%_2PI%, th %%= %_2PI%, t=th+= th>>31&%_2PI%, s1=(t-%_PI#2%^t-%_3PI#2%)>>31, s3=%_3PI#2_1%-t>>31, t=(-t&s1)+(t&~s1)+(%_PI%&s1)+(-%_2PI%&s3), #S=%_SIN%, t=%_COS%, #C=(-t&s1)+(t&~s1), $x=%_XC%-#C, $y=%_YC%-#S"
+		set /a "th+=-%_SPEED%+%_2PI%, th%%=%_2PI%, t=th+=th>>31&%_2PI%, s1=(t-%_PI#2%^t-%_3PI#2%)>>31, s3=%_3PI#2_1%-t>>31, t=(-t&s1)+(t&~s1)+(%_PI%&s1)+(-%_2PI%&s3), #S=%_SIN%, t=%_COS%, #C=(-t&s1)+(t&~s1), $x=%_XC%-#C, $y=%_YC%-#S"
 
 		REM set /a "hue=!random! %% 256"
 
-		set "$pin=!$pin!%_ESC%[38;5;!hue!m"
 		for /l %%a in (0 1 %_HALF_WID%) do (
 			set /a "#x=($x+=#C)/10000+1, #y=($y+=#S)/10000+1"
-			REM set "$pin=!$pin!%_ESC%[!#x!;!#y!H%_ESC%[38;5;!hue!m@"
-			set "$pin=!$pin!%_ESC%[!#x!;!#y!H@"
+			set "$pin=%_ESC%[!#x!;!#y!H@!$pin!"
 		)
+		set "$pin=%_ESC%[38;5;!hue!m!$pin!"
 
 		<nul set /p "=!$erase_last_pin!!$pin!"
 
@@ -59,7 +47,6 @@ set /a "hue=88"
 		set "$pin="
 
 		REM title %%i / 500000; th=!th!; _DTH=%_DTH%;  {hue=!hue!}
-
 	)
 )
 
