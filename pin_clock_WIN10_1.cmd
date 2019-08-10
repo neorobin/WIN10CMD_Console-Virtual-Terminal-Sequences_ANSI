b44cc8d9c5bffdb7b4b20578a484eb8e0e206a2d
neorobin <neorobin@163.com>
2019-08-10 13:15:07 +0800

    Update pin_clock_WIN10_1.cmd

--- pin_clock_WIN10_1.cmd
+++ pin_clock_WIN10_1.cmd
@@ -115,7 +115,7 @@ set /a "_DENSITY=150,  _SPEED=%_2PI%/_DENSITY, _SPEED=3*%_DEG%, th=_TH0+%_2PI%, 
 
 	REM nail up scale
 	<nul set /p "=%_ESC%[48;2;%_RGB_FACE%m"
-	for /L %%i in (0 1 11) do (
+	for /L %%i in (0 1 3) do (
 			set /a "r3=%%i %% 3"
 			set /a "th=%_PI% - %%i*%_2PI%/12, th%%=%_2PI%, t=th+=th>>31&%_2PI%, s1=(t-%_PI#2%^t-%_3PI#2%)>>31, s3=%_3PI#2_1%-t>>31, t=(-t&s1)+(t&~s1)+(%_PI%&s1)+(-%_2PI%&s3), #S=%_SIN%, t=%_COS%, #C=(-t&s1)+(t&~s1), $x=%_XCZOOM%-#C, $y=%_YCZOOM%-#S"
 
@@ -123,18 +123,18 @@ set /a "_DENSITY=150,  _SPEED=%_2PI%/_DENSITY, _SPEED=3*%_DEG%, th=_TH0+%_2PI%, 
 				set /a "#x=($x+=#C)/10000+1, #y=($y+=#S)/10000+1, #x_=%_2XC%-#x, #y_=%_2YC%-#y"
 				if !r3!==0 (
 					if %%a geq %_R_FACE_2% if %%a lss %_R_FACE% (
-						set "$pin=%_ESC%[!#x!;!#y!H%_PEN_SCALE%!$pin!"
+						set "$pin=%_ESC%[!#x!;!#y!H%_PEN_SCALE%%_ESC%[!#x_!;!#y_!H%_PEN_SCALE%%_ESC%[!#x_!;!#y!H%_PEN_SCALE%%_ESC%[!#x!;!#y_!H%_PEN_SCALE%!$pin!"
 					)
 				) else (
 					if %%a equ %_R_FACE_1% (
-						set "$pin=%_ESC%[!#x!;!#y!H%_PEN_SCALE%!$pin!"
+						set "$pin=%_ESC%[!#x!;!#y!H%_PEN_SCALE%%_ESC%[!#x_!;!#y_!H%_PEN_SCALE%%_ESC%[!#x_!;!#y!H%_PEN_SCALE%%_ESC%[!#x!;!#y_!H%_PEN_SCALE%!$pin!"				
 					)
 				)
 			)
 			set "$pin=%_ESC%[38;2;%_RGB_SCALE%m!$pin!"
 			<nul set /p "=!$erase_last_pin!!$pin!"
 			set "$pin="
-			title nail up scale %%i / 11
+			title nail up scale %%i / 2
 	)
 
 	<nul set /p "=%_ESC%[48;2;%_RGB_FACE%m"
