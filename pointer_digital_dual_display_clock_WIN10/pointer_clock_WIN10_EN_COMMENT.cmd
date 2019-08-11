53272770fc8cdf208096b0d62dccb80c0cabc5df
neorobin <neorobin@163.com>
2019-08-11 22:43:21 +0800

    Update pointer_clock_WIN10_EN_COMMENT.cmd

--- pointer_digital_dual_display_clock_WIN10/pointer_clock_WIN10_EN_COMMENT.cmd
+++ pointer_digital_dual_display_clock_WIN10/pointer_clock_WIN10_EN_COMMENT.cmd
@@ -6,7 +6,7 @@
 ::
 ::  author: neorobin ver: 20190811_222058
 ::
-::  https://github.com/neorobin/WIN10CMD_Console-Virtual-Terminal-Sequences_ANSI/blob/master/pointer_digital_dual_display_clock_WIN10/pointer_clock_WIN10.cmd
+::  https://github.com/neorobin/WIN10CMD_Console-Virtual-Terminal-Sequences_ANSI/blob/master/pointer_digital_dual_display_clock_WIN10/pointer_clock_WIN10_EN_COMMENT.cmd
 ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
